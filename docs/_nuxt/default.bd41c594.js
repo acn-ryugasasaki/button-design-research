dfdeff5ecd3acfcbe614f9ea05d3f581bb4c2ab5
acn-ryugasasaki <ryuga.sasaki@accenture.com>
2023-12-20 23:34:56 +0900

    [add] docs

---
+++ docs/_nuxt/default.bd41c594.js
@@ -0,0 +1,1 @@
+import{_ as t,v as n,L as r,t as a}from"./entry.828d39f5.js";const o={};function s(e,c){return a(),n("main",null,[r(e.$slots,"default")])}const _=t(o,[["render",s]]);export{_ as default};
